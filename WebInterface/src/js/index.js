398eacc1f1bb593baff4b096a92d87972e6c86d5
TrueKuehli <rctcoaster2000@hotmail.de>
2019-08-31 16:57:38 +0200

    Started redesign

--- WebInterface/src/js/index.js
+++ WebInterface/src/js/index.js
@@ -1,10 +1,28 @@
-import Interface from './modules/interface';
-import UIManager from './modules/ui/uiManager';
-import Networker from './modules/networking/networker';
+import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCDrawer} from '@material/drawer';
+// import {MDCTabBar} from '@material/tab-bar';
 
-let iface = new Interface();
-let uiMan = new UIManager(iface);
-uiMan.initLogin();
+// import Interface from './modules/interface';
+// import UIManager from './modules/ui/uiManager';
+// import Networker from './modules/networking/networker';
 
-let netMan = new Networker(iface);
-netMan.getServers();
+
+const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+const navigationDrawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+topAppBar.setScrollTarget(document.getElementById('main-content'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
+  navigationDrawer.open = !navigationDrawer.open;
+});
+document.getElementById('main-content').addEventListener('click', () => navigationDrawer.open = false);
+
+
+// const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
+// const iface = new Interface();
+// const uiMan = new UIManager(iface);
+// uiMan.initLogin();
+// const netMan = new Networker(iface);
+// netMan.getServers();
+
+// TODO: Implement URL-encoded starting position for games tab
