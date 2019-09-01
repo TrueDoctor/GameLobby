da582f77dd758c9647be92a13ffd0bb9abc9df23
TrueKuehli <rctcoaster2000@hotmail.de>
2019-09-01 13:34:20 +0200

    Implemented Tab Bar

--- WebInterface/src/js/index.js
+++ WebInterface/src/js/index.js
@@ -1,6 +1,6 @@
 import {MDCTopAppBar} from '@material/top-app-bar';
 import {MDCDrawer} from '@material/drawer';
-// import {MDCTabBar} from '@material/tab-bar';
+import {MDCTabBar} from '@material/tab-bar';
 
 // import Interface from './modules/interface';
 // import UIManager from './modules/ui/uiManager';
@@ -9,15 +9,21 @@ import {MDCDrawer} from '@material/drawer';
 
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const navigationDrawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 
 topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
   navigationDrawer.open = !navigationDrawer.open;
 });
 document.getElementById('main-content').addEventListener('click', () => navigationDrawer.open = false);
+tabBar.listen('MDCTabBar:activated', (event) => {
+  for (let list of document.getElementsByClassName('games-listing')) {
+    list.classList.add('hidden');
+  }
+  document.getElementById('listing_' + event.detail.index).classList.remove('hidden');
+});
 
 
-// const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 
 // const iface = new Interface();
 // const uiMan = new UIManager(iface);
